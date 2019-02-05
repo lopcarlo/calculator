434bb0472083ffc1f78c0ca26514d4866533c4f8
Luis Lopes <lopcarlo@gmail.com>
2019-02-05 17:15:42 +0000

    Sci Calculator

--- src/org/academiadecodigo/bootcamp/calculator/Playground.java
+++ src/org/academiadecodigo/bootcamp/calculator/Playground.java
@@ -8,37 +8,20 @@ public class Playground {
         Calculator c1 = new Calculator("Texas", "Black");
         System.out.println("Original Brand is: "+ c1.getBrand() + " and color: " + c1.getColor());
 
-        Calculator c2 = new Calculator("Casio", "Blue");
-        System.out.println("Original Brand is: "+ c2.getBrand() + " and color: " + c2.getColor());
+        SciCalc s1= new SciCalc("Casio", "Blue");
 
-        c2.setColor("Green");
+
+
 
         System.out.println("Calculator is a " + c1.getBrand() + " and its " + c1.getColor());
-        System.out.println("Calculator is a " + c2.getBrand() + " and its " + c2.getColor());
+
+        System.out.println("--------------SCICALC---------------");
+
+        System.out.println(s1.power(2,3));
+        System.out.println(s1.add(5, 10));
+        System.out.println(s1.log(5));
 
 
-        System.out.println(c1.add(3, 8));
-        System.out.println("Battery Level " + c1.getBatterie());
-        System.out.println(c1.divide(10, 2.0));
-        System.out.println("battery level " + c1.getBatterie());
-        System.out.println(c2.add(20, 50) + " " + "Battery level "+ c2.getBatterie());
-        System.out.println("Recharging" + c1.recharge());
-
-        System.out.println(c1.subtract(10, 5));
-        System.out.println("battery level " + c1.getBatterie());
-        System.out.println(c2.add(20, 50) + " " + "Battery level " + c2.getBatterie());
-        System.out.println(c1.multiply(10, 5));
-        System.out.println("battery level " + c1.getBatterie());
-        System.out.println("Recharging" + c1.recharge());
-        System.out.println(c2.add(20, -50) + " " + "Battery level " + c2.getBatterie());
-
-        System.out.println("battery level " + c1.getBatterie());
-        System.out.println(c2.add(32, 4));
-
-        System.out.println(c1.multiply(10.1, 5));
-        System.out.println(c1.multiply(10, 5));
-        System.out.println("battery level " + c1.getBatterie());
-        System.out.println(c2.add(20, 50) + " " + "Battery level "+ c2.getBatterie());
     }
 
 }
---
+++ src/org/academiadecodigo/bootcamp/calculator/SciCalc.java
@@ -0,0 +1,29 @@
+package org.academiadecodigo.bootcamp.calculator;
+
+/**
+ * Created by codecadet on 05/02/2019.
+ */
+public class SciCalc extends Calculator{
+
+    public SciCalc(String brand, String color) {
+        super(brand, color);
+    }
+
+    public double power(double number, double power ){
+        return Math.pow(number,power);
+    }
+
+    public double log (double a){
+        return Math.log(a);
+    }
+
+
+    public double sin (double n){
+        return Math.sin(n);
+    }
+
+
+
+
+
+}
